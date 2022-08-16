bfd7be2c59398c4d89d5c3d332d847f896f22a08
dusterbloom <giuseppe@informal.systems>
2022-08-16 13:01:10 +0200

    Layout change3

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: [require("./styles/daisyui-themes.json")],
-   
-    //themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
-    
-  
+    themes: [require("./styles/daisyui-themes.json")],    
+  },
 };
