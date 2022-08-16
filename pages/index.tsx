7a50f254f2bd7f17cc74286ff5bf642beb06eb93
dusterbloom <giuseppe@informal.systems>
2022-08-16 15:37:02 +0200

    frontpage 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,8 @@ const Home: NextPage = () => {
   return (
     <WalletLoader>
       <h1 className="text-6xl font-bold">
-        Welcome to {process.env.NEXT_PUBLIC_CHAIN_NAME} !
+        Welcome
+        {/* {process.env.NEXT_PUBLIC_HOME_HEADER} ! */}
       </h1>
 
       <div className="mt-3 text-2xl">
@@ -19,18 +20,6 @@ const Home: NextPage = () => {
           {walletAddress}
         </pre>
       </div>
-
-      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
-        <Link href="/send" passHref>
-          <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
-            <h3 className="text-2xl font-bold">Transfer to wallet &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Execute a transaction to send funds to a wallet or contract address.
-            </p>
-          </a>
-        </Link>
-      </div>
-    
     
     <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
     <Link href="/create" passHref>
@@ -41,6 +30,16 @@ const Home: NextPage = () => {
         </p>
       </a>
     </Link>
+    <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 max-w-full sm:w-full">
+        <Link href="/send" passHref>
+          <a className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus">
+            <h3 className="text-2xl font-bold">Transfer to wallet &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Execute a transaction to send funds to a wallet or contract address.
+            </p>
+          </a>
+        </Link>
+      </div>
   </div>
   </WalletLoader>
   );
