9e02bdc58f25e8a63b9edd4d193fd2596d3e744a
dusterbloom <giuseppe@informal.systems>
2022-08-16 14:59:11 +0200

    layout changes3

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,5 +19,8 @@ module.exports = {
   plugins: [require("daisyui")],
   daisyui: {
     themes: [require("./styles/daisyui-themes.json")],    
+    primary: "blue",
+    "primary-focus": "mediumblue",
+
   },
 };
