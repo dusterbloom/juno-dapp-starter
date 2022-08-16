fa5435e58a0fa7590e69fcb32e4639056e1eb25e
dusterbloom <giuseppe@informal.systems>
2022-08-16 15:25:28 +0200

    frontpage 2

--- components/WalletLoader.tsx
+++ components/WalletLoader.tsx
@@ -35,20 +35,13 @@ function WalletLoader({
         </h1>
 
         <p className="mt-3 text-2xl">
-          Here businesses save money by clearing their obligations together. {" "}
-          {/* 
-          <a
-            className="pl-1 link link-primary link-hover"
-            href="https://keplr.app/"
-          >
-            Keplr wallet
-          </a> */}
+          Where businesses save money by {" "}
           <a
             className="pl-1 link link-primary link-hover"
             href="https://obligatto.app/how-it-works/#"
           >
-          Pay what you owe with trade credit owed to you. 
-          </a>
+          by clearing their obligations together.
+        </a>
         </p>
 
         <div className="flex flex-wrap items-center justify-around md:max-w-4xl mt-6 sm:w-full">
@@ -56,12 +49,25 @@ function WalletLoader({
             className="p-6 mt-6 text-left border border-secondary hover:border-primary w-96 rounded-xl hover:text-primary focus:text-primary-focus"
             onClick={connectWallet}
           >
-            <h3 className="text-2xl font-bold">Connect your wallet &rarr;</h3>
+            <h3 className="text-2xl font-bold">Start &rarr;</h3>
             <p className="mt-4 text-xl">
               Connect your Keplr wallet and start clearing away your obligations!
             </p>
           </button>
         </div>
+      
+        <p className="mt-3 text-2">
+          If you need a wallet, install {" "}   
+          <a
+            className="pl-1 link link-primary link-hover"
+            href="https://keplr.app/"
+          >
+            Keplr wallet
+          </a>
+        </p>
+        
+  
+
       </div>
     );
   }
