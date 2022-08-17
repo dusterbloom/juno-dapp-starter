29c603b2e3c173e6379474e246dca397a08f7eee
dusterbloom <giuseppe@informal.systems>
2022-08-17 12:27:07 +0200

    fixing denom 5

--- pages/create.tsx
+++ pages/create.tsx
@@ -158,7 +158,7 @@ return (
             value={edgeAmount}
           />
            <span className="absolute top-0 right-0 bottom-0 px-4 py-5 rounded-r-full bg-secondary text-base-100 text-sm">
-           {denom}
+           {/* {denom} */}
 
           </span>
         </div>
