66158aa0b0b80f63c932efbe1ed995979d489db5
dusterbloom <giuseppe@informal.systems>
2022-09-12 16:13:16 +0200

    rendering working with array

--- pages/clear.tsx
+++ pages/clear.tsx
@@ -29,6 +29,8 @@ const PUBLIC_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || "was
 
 
 const Clear: NextPage = () => {
+  console.log('clear called')
+
   const { walletAddress, signingClient } = useSigningClient();
   const [balance, setBalance] = useState("");
   const [loadedAt, setLoadedAt] = useState(new Date());
@@ -39,10 +41,8 @@ const Clear: NextPage = () => {
   const [memo, setMemo] = useState("");
   const [denom,setDenom] = useState("");
   const [owner,setOwner] = useState("");
-  const [edges,setEdges] = useState("");
-  const [currentRow, setCurrentRow] = useState(null);
-  const [newData, setnewData] = useState([]);
-
+  const [edges,setEdges] = useState([{ debtor: '', creditor: '' , amount: 0, edge_id: 0, graph_id: 0}]);
+  
 
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
@@ -62,13 +62,43 @@ const Clear: NextPage = () => {
         setBalance(
           `${convertMicroDenomToDenom(amount)} ${convertFromMicroDenom(denom)}`
         );
+        console.log("balance", response)
       })
       .catch((error) => {
         setError(`Error! ${error.message}`);
         console.log("Error signingClient.getBalance(): ", error);
       });
-      
-  
+
+       // Get all edges 
+  const QueryMsg3 = {
+    all_edges: {}
+  }
+
+
+// Get all edges
+    signingClient
+    ?.queryContractSmart(PUBLIC_CONTRACT_ADDRESS, QueryMsg3)
+    .then((response: any) => {
+      console.log("edges", response.edges);
+      setEdges(response.edges)
+      // console.log("edges", response);
+      // const data = response;
+    //  setEdges(`${response}`);
+      //  setEdges(
+      //   [`${(edge_id)} ${(debtor)} ${(creditor)} ${(amount)} ${(graph_id)}`]
+      // );
+      setSuccess("");
+      setLoading(false);
+      // setSuccess(denom);
+    })
+    .catch((error) => {
+      // setLoading(false);
+      setError(`Error! ${error.message}`);
+      console.log("Error signingClient.execute(): ", error);
+    });
+
+
+
 
 }, [signingClient, walletAddress, loadedAt]);
 
@@ -92,56 +122,29 @@ const Clear: NextPage = () => {
 //   });
   
    // Get the network owner
-  const QueryMsg2 = {
-    get_owner: {}
-  }
+  // const QueryMsg2 = {
+  //   get_owner: {}
+  // }
 
 
-  signingClient
-  ?.queryContractSmart(PUBLIC_CONTRACT_ADDRESS, QueryMsg2)
-  .then((response) => {
-    console.log("owner", response.owner);
-    setOwner(`${response.owner}`);
-    //const denomB = $(response.de)
-    // setSuccess(denom);
-  })
-  .catch((error) => {
-    setLoading(false);
-    setError(`Error! ${error.message}`);
-    console.log("Error signingClient.execute(): ", error);
-  });
+  // signingClient
+  // ?.queryContractSmart(PUBLIC_CONTRACT_ADDRESS, QueryMsg2)
+  // .then((response) => {
+  //   console.log("owner", response.owner);
+  //   setOwner(`${response.owner}`);
+  //   //const denomB = $(response.de)
+  //   // setSuccess(denom);
+  // })
+  // .catch((error) => {
+  //   setLoading(false);
+  //   setError(`Error! ${error.message}`);
+  //   console.log("Error signingClient.execute(): ", error);
+  // });
     
-  // Get all edges 
-  const QueryMsg3 = {
-    all_edges: {}
-  }
+ 
 
 
-  signingClient
-  ?.queryContractSmart(PUBLIC_CONTRACT_ADDRESS, QueryMsg3)
-  .then((response) => {
-    console.log("edges", response);
-    setEdges(`${response}`);
-    // setSuccess(edges)
-    //const denomB = $(response.de)
-    // setSuccess(denom);
-  })
-  .catch((error) => {
-    setLoading(false);
-    setError(`Error! ${error.message}`);
-    console.log("Error signingClient.execute(): ", error);
-  });
-
-
- Object.entries(edges);
-
-const data = Object.entries(QueryMsg3);
-
-//  newData.map() => 
-
-
-
-
+    
 //   const handleCreate = (event: MouseEvent<HTMLElement>) => {
 //     event.preventDefault();
 //     setError("");
@@ -196,16 +199,22 @@ return (
       <h1 className="text-5xl font-bold my-8">
         All obligations
       </h1>
-   
-
-    
 
         <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                 <thead className="text-xs text-gray-500 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" className="py-3 px-6">
-                            Debtor
+                    <th scope="col" className="py-3 px-6">
+                            <div className="flex items-center">
+                              Id
+                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
+                            </div>
+                        </th>  
+                    <th scope="col" className="py-3 px-6">
+                            <div className="flex items-center">
+                                Debtor
+                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
+                            </div>
                         </th>
                         <th scope="col" className="py-3 px-6">
                             <div className="flex items-center">
@@ -215,76 +224,41 @@ return (
                         </th>
                         <th scope="col" className="py-3 px-6">
                             <div className="flex items-center">
-                                Graph
-                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
-                            </div>
-                        </th>
-                        <th scope="col" className="py-3 px-6">
-                            <div className="flex items-center">
                                 Amount
                                 <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
                             </div>
                         </th>
-                        {/* <th scope="col" className="py-3 px-6">
+                        <th scope="col" className="py-3 px-6">
                             <span className="sr-only">Edit</span>
-                        </th> */}
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
+
+                  {edges.map(e => 
                     <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         {/* <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                             Apple MacBook Pro 17"
                         </th> */}
                         <th scope="row" className="py-4 px-6">
-
+                        <td className="py-4 px-6">
+                        {e.edge_id}
+                        </td>
                         </th>
                         <td className="py-4 px-6">
-                            Sliver
+                           { e.debtor }
                         </td>
                         <td className="py-4 px-6">
-                            Laptop
+                            {e.creditor}
                         </td>
                         <td className="py-4 px-6">
-                            amount
+                          {e.amount}
                         </td>
                         <td className="py-4 px-6 text-right">
                             <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
                         </td>
                     </tr>
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                        <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
-                        </th>
-                        <td className="py-4 px-6">
-                            White
-                        </td>
-                        <td className="py-4 px-6">
-                            Laptop PC
-                        </td>
-                        <td className="py-4 px-6">
-                            $1999
-                        </td>
-                        <td className="py-4 px-6 text-right">
-                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                    </tr>
-                    <tr className="bg-white dark:bg-gray-800">
-                        <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
-                        </th>
-                        <td className="py-4 px-6">
-                            Black
-                        </td>
-                        <td className="py-4 px-6">
-                            Accessories
-                        </td>
-                        <td className="py-4 px-6">
-                            $99
-                        </td>
-                        <td className="py-4 px-6 text-right">
-                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td>
-                    </tr>
+                  )}
                 </tbody>
             </table>
         </div>
