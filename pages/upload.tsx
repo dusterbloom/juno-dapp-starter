e41a68f93a8d6497ca26a60dc00b544871a88199
dusterbloom <giuseppe@informal.systems>
2022-10-13 11:49:09 +0200

    UI improvement for upload

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -45,8 +45,6 @@ const Upload: NextPage = () => {
   const [balance, setBalance] = useState("");
   const [loadedAt, setLoadedAt] = useState(new Date());
   const [loading, setLoading] = useState(false);
-
-
   const [success, setSuccess] = useState("");
   const [error, setError] = useState("");
   const [parsedCsvData, setParsedCsvData] = useState<Graph[]>([]);
@@ -85,14 +83,19 @@ const Upload: NextPage = () => {
   const {
     getRootProps,
     getInputProps,
-    isDragActive,
-    isDragAccept,
-    isDragReject,
+    open,
+    acceptedFiles
   } = useDropzone({
     onDrop,
     // Accept?: 'text/csv',
   });
 
+
+  const files = acceptedFiles.map(file => (
+    <li key={file.name}>
+      {file.name} - {file.size} bytes
+    </li>
+  ));
 
   // Loads the wallet and converts the balance
   useEffect(() => {
@@ -163,93 +166,31 @@ const Upload: NextPage = () => {
         Upload obligations
       </h1>
 
-      {/* <div className="overflow-x-auto relative shadow-md sm:rounded-lg"> */}
+  
+
+    
+        
+        
         <div
         {...getRootProps({
           className: `dropzone 
-          ${isDragAccept && 'dropzoneAccept'} 
-          ${isDragReject && 'dropzoneReject'}`,
+          ${'dropzoneAccept'} 
+          ${'dropzoneReject'}`,
         })}
-      >
+        >
+    
         <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
+        {(
           <p className="mt-1 md:mt-0 btn btn-secondary btn-small font-semibold hover:text-base-100 text-2xl  flex-grow"
         >Select a CSV file.</p>
         )}
       </div>
 
         <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-500 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                    <th scope="col" className="py-3 px-6">
-                            <div className="flex items-center">
-                              Id
-                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
-                            </div>
-                        </th>  
-                    <th scope="col" className="py-3 px-6">
-                            <div className="flex items-center">
-                                Debtor
-                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
-                            </div>
-                        </th>
-                        <th scope="col" className="py-3 px-6">
-                            <div className="flex items-center">
-                                Creditor
-                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
-                            </div>
-                        </th>
-                        <th scope="col" className="py-3 px-6">
-                            <div className="flex items-center">
-                                Amount
-                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
-                            </div>
-                        </th>
-                        <th scope="col" className="py-3 px-6">
-                            <div className="flex items-center">
-                                Graph
-                                <a href="#"><svg xmlns="http://www.w3.org/2000/svg" className="ml-1 w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 320 512"><path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z"></path></svg></a>
-                            </div>
-                        </th>
-                        {/* <th scope="col" className="py-3 px-6">
-                            <span className="sr-only">Graph</span>
-                        </th> */}
-                    </tr>
-                </thead>      
-                <tbody>
-
-                  
-                    { parsedCsvData.map((e, index) => (
-                    <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" >
-
-                        <th scope="row" className="py-4 px-6">
-                        <td className="py-4 px-6">
-                        {e.edge_id}
-                        </td>
-                        </th>
-                        <td className="py-4 px-6">
-                           { e.debtor }
-                        </td>
-                        <td className="py-4 px-6">
-                            {e.creditor}
-                        </td>
-                        <td className="py-4 px-6">
-                          {e.amount}
-                        </td>
-                        <td className="py-4 px-6">
-                          {e.graph_id}
-                        </td>
-                        {/* <td className="py-4 px-6 text-right">
-                            <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                        </td> */}
-                    </tr>
-                  ))}
-                
-                </tbody>
-            </table>
+        <aside>
+        <h4>Files ready for upload</h4>
+        <ul className='alert alert-success'>{files}</ul>
+      </aside>
         </div>
 
     
@@ -259,7 +200,7 @@ const Upload: NextPage = () => {
           className="mt-4 md:mt-0 btn btn-primary btn-lg font-semibold hover:text-base-100 text-2xl rounded-full flex-grow"
           onClick={handleUpload}
         >
-          Upload obligations
+          Upload file
         </button>
         
       
